fa1bfbb6c1133c1be321cf3c7a6b6e7e8aad4c7a
GodUsopp <an.brendancode@gmail.com>
2022-07-14 21:03:41 -0700

    Add eventlisteners and touch up on css and html
    Made slight changes to html and css. Gave operator buttons eventlisteners.

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 const display = document.querySelector(".display");
 const numbers = document.querySelectorAll(".number");
+const backSpace = document.querySelector(".backspace");
+const clear = document.querySelector(".clear");
+const equals = document.querySelector(".equals");
+const operator = document.querySelectorAll(".operator");
+const negative = document.querySelector(".negative");
+//const subtractOperator = document.querySelector(".subtract");
+//const multiplyOperator = document.querySelector(".multiply");
+//const divideOperator = document.querySelector(".divide");
+
+let num;
+let num2;
+let operation = "";
 
 function add(a, b) {
   return a + b;
@@ -22,20 +34,59 @@ function calculate(a, b, c) {
     return add(a, b);
   } else if (c == "-") {
     return subtract(a, b);
-  } else if (c == "*") {
+  } else if (c == "X") {
     return multiply(a, b);
   } else {
     return divide(a, b);
   }
 }
 
-function clear() {
+function makeNegative(a) {
+  a = -a;
+  if (a < 0) a = +a;
+}
+
+function clearDisplay() {
   display.textContent = "0";
+  num = 0;
+  num2 = 0;
+  operation = "";
 }
 
 numbers.forEach((number) => {
   number.addEventListener("click", () => {
     if (display.textContent == 0) display.textContent = "";
-    display.textContent += number.value;
+    display.textContent += number.textContent;
+    if (operation === "") {
+      num = display.textContent;
+    } else {
+      num2 = display.textContent;
+    }
   });
 });
+
+clear.addEventListener("click", () => {
+  clearDisplay();
+  //num = display.textContent;
+});
+
+equals.addEventListener("click", () => {
+  num = parseInt(num);
+  num2 = parseInt(num2);
+  result = calculate(num, num2, operation);
+  display.textContent = result;
+  num = result;
+});
+
+operator.forEach((operator) => {
+  operator.addEventListener("click", () => {
+    operation = operator.textContent;
+    display.textContent = "0";
+  });
+});
+
+negative.addEventListener("click", () => {
+  makeNegative(display.textContent);
+});
+
+backSpace.addEventListener("click", () => {});
